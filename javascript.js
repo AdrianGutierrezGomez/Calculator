68ed9bc91a1f85d9122e40f34937b43d6bd3248f
Adrian Gutierrez Gomez <adriangtzg.reverie@gmail.com>
2023-01-02 18:28:41 -0600

    Add buttons and basic eventlisteners

---
+++ javascript.js
@@ -0,0 +1,81 @@
+const zeroBtn = document.getElementById('0');
+const dotBtn = document.getElementById('.');
+const tenTimesBtn = document.getElementById('x10^x');
+const ansBtn = document.getElementById('ans');
+const equalsBtn = document.getElementById('=');
+const oneBtn = document.getElementById('1');
+const twoBtn = document.getElementById('2');
+const threeBtn = document.getElementById('3');
+const plusBtn = document.getElementById('+');
+const minusBtn = document.getElementById('-');
+const fourBtn = document.getElementById('4');
+const fiveBtn = document.getElementById('5');
+const sixBtn = document.getElementById('6');
+const multiplicationBtn = document.getElementById('x');
+const divisionBtn = document.getElementById('/');
+const sevenBtn = document.getElementById('7');
+const eightBtn = document.getElementById('8');
+const nineBtn = document.getElementById('9');
+const delBtn = document.getElementById('DEL');
+const acBtn = document.getElementById('AC');
+
+zeroBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+dotBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+tenTimesBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+ansBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+equalsBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+oneBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+twoBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+threeBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+plusBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+minusBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+fourBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+fiveBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+sixBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+multiplicationBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+divisionBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+sevenBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+eightBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+nineBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+delBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
+acBtn.addEventListener('click', () => {
+    alert("Hello World");
+  });
